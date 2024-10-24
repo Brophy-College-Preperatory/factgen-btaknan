faf7b7f09bcebbe34d2db44e213d186dffb2dbe8
btaknan <btaknan28@brophybroncos.org>
2024-10-24 06:43:05 +0000

    done

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-var factList = [
- "Fact 1",
- "Fact 2", 
- "Fact 3", 
- "Fact 4"];
-
-var fact = document.getElementById("fact");
-var myBtn = document.getElementById("myBtn");
-var count = 0;
-
-myBtn.addEventListener("click", displayFact);
-
-function displayFact(){
-  fact.innerHTML = factList[count];
-  count++;
-  if (count == factList.length){
-    count = 0;
-  }
-}
+var factList = [ //has a list of facts
+  "Fact 1",
+  "Fact 2", 
+  "Fact 3", 
+  "Fact 4"];
+ 
+ var fact = document.getElementById("fact"); //where the facts will show up
+ var myBtn = document.getElementById("myBtn"); //button to show the facts
+ var count = 0; //makes a counter
+ 
+ myBtn.addEventListener("click", displayFact); //adds click event listener for the button
+ 
+ function displayFact(){ //function to display a fact
+   fact.innerHTML = factList[count]; //fact to count
+   count++; //a count to move on to another fact
+   if (count == factList.length){ //checks if the count reached all of the list
+     count = 0; //resets the count to 0
+   }
+ }
+ 
